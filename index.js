71f5f9b56d83b3d4c703790dd9f16cd4efa43e7f
TJ Stifter <tjstifter@gmail.com>
2023-05-04 17:16:55 -0400

    passes test

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards (listNames, event){
+    const messages=[];
+
+    for (let i = 0; i<listNames.length; i++){
+        messages.push(`Thank you, ${listNames[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+/*function countDown(number){
+    for (let i = 0; i <= number; i++){
+        console.log (number - i);
+    }
+}*/
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number)
+        number -= 1;
+    }
+}
